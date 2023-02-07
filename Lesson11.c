d801215a3859556c1913e56c686b935707d8f60f
Sasayi <alexrt9375@gmail.com>
2023-02-07 22:58:24 +0300

    add new file1

--- Lesson11.c
+++ Lesson11.c
@@ -3,8 +3,12 @@
 int main(int argc, char* argv[])
 {
     int x = 1;
-    signed int y = 2;
+    int y = 2;
     int z = x + y;
     printf("z = %d\n", z);
+    int x1 = 1;
+    int y1 = -1;
+    int z1 = x1 + y1;
+    printf("z1 = %d\n", z1);
     return 0;
 }
