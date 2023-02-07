19fcef131d61133c3212e4368026de5cf459b464
Sasayi <alexrt9375@gmail.com>
2023-02-08 01:53:15 +0300

    Update Lesson11.c

--- Lesson11.c
+++ Lesson11.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-
+#include <stdbool.h>
+#include <inttypes.h>
 int main(int argc, char* argv[])
 {
-    int x = 1;
-    int y = 2;
-    int z = x + y;
+    printf("x?\n");
+    int x[10];
+    scanf("%d", x);
+    printf("y?\n");
+    int y[10];
+    scanf("%d", y);
+    int z = *x+*y;
     printf("z = %d\n", z);
-    int x1 = 1;
-    int y1 = -1;
-    int z1 = x1 + y1;
-    printf("z1 = %d\n", z1);
     return 0;
 }
+
+
