fbc65eea32322a03e5c2595109702e0a21a5a08e
Sasayi <alexrt9375@gmail.com>
2023-02-07 23:13:45 +0300

    Update lessons13

--- Lesson13.c
+++ Lesson13.c
@@ -8,6 +8,10 @@ int main(int argc, char* argv[])
     x = (-1 > 1) + (1 <= -1);
 
     printf("x = %d\n", x);
+    int y;
 
+    y = (10 > -3) + (-3 <= 10);
+
+    printf("y = %d\n", y);
     return 0;
 }
