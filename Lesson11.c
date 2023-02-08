3bafc67b3d931112c58ba4d34336a1e5dd0cae48
Sasayi <alexrt9375@gmail.com>
2023-02-08 23:11:40 +0300

    Update Lesson11.c

--- Lesson11.c
+++ Lesson11.c
@@ -3,13 +3,9 @@
 #include <inttypes.h>
 int main(int argc, char* argv[])
 {
-    printf("x?\n");
-    int x[1];
-    scanf("%d", x);
-    printf("y?\n");
-    int y[1];
-    scanf("%d", y);
-    int z = *x+*y;
+    int x, y;
+    scanf("%d %d", &x, &y);
+    int z = x+y;
     printf("z = %d\n", z);
     return 0;
 }
