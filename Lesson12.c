679faddbb6bed6afb94f9d1675513c67078c6da5
Sasayi <alexrt9375@gmail.com>
2023-02-08 23:55:00 +0300

    Update Lesson12.c

--- Lesson12.c
+++ Lesson12.c
@@ -5,16 +5,12 @@ int main(int argc, char* argv[])
     int x, y;
     scanf("%d %d", &x, &y);
 if(x > y)
-printf("%d\n", 2);
-else
-  printf("%d\n", 1);
-  if(x < y)
-   printf("%d\n", 1);
+printf("%d\n", 1);
 else
   printf("%d\n", 2);
-   if(x == y)
+if(x == y)
    printf("%d\n",0);
 else
-  printf("%d\n",0);
+  printf("%d\n",3);
   return 0;
     }
