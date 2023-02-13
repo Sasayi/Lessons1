5f11ab7ac1ed09c3b577c58462e9634936218a5e
Sasayi <alexrt9375@gmail.com>
2023-02-13 23:04:17 +0300

    Update Lesson13.c

--- Lesson13.c
+++ Lesson13.c
@@ -4,22 +4,18 @@ int main(int argc, char* argv[])
 {
     int x;
     scanf("%d", &x);
-    while (x !=0)
-{
-   if (x%100 == 0)
-        printf ("Year %d is no. \n",x);
-        else
-        printf ("Year %d is yes. \n",x);
-        if (x%4 == 0)
+while (x !=0)
+  {
+       if (x%4 == 0)
             printf ("Year %d is yes. \n",x);
+        else
+            if (x%100 != 0)
+        printf ("Year %d is no. \n",x);
             else
-            printf ("Year %d is no. \n",x);
-        if (x%400 == 0)
+                if (x%400 == 0)
                 printf ("Year %d is yes. \n",x);
-                else
-                printf ("Year %d is no. \n",x);
-            scanf("%d", &x);
-}
+                           scanf("%d", &x);
+  }
 
   return 0;
     }
